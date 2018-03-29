38e1db8af680fec250c0f87f206a9fb489002faf
luckyscript <jsjhlk@qq.com>
2018-03-29 23:11:36 +0800

    5

--- 5/5-1.c
+++ 5/5-1.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 int
 getint(int *p) {
     int c;
--- 5/5-2.c
+++ 5/5-2.c
@@ -1,6 +1,26 @@
+#include <stdio.h>
 int
 getFloat (float *p) {
     int c;
+    float power = 0;
     while(isspace(c = getch()))
     ;
+    if(!isdigit(c) && c != EOF && c != '-' && c != '+') {
+        ungetch(c);
+        return 0;
+    }
+    int sign;
+    sign = (c == '-')? -1: 1;
+    for (*p = 0; isdigit(c) || c == '.'; c = getch()) {
+        if(power) power*=10.0;
+        if (c == '.') {
+            power = 1.0;
+        }
+        *p = 10.0 * *p + (c - '0');
+    }
+    *p *= sign / power;
+    if (c != EOF) {
+        ungetch(c);
+    }
+    return c;
 }
